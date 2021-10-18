55c5492c32b75184995294d1c91bf206644cfe20
Youngking <youngking1686@gmail.com>
2021-10-18 10:08:17 +0530

    db_load fix

--- db_load.py
+++ db_load.py
@@ -9,7 +9,7 @@ def format_fix(start_time):
     return start_time.replace("9", "09") if start_time.split(':')[0] == '9' else start_time
 
 def data_load():
-    df = pd.read_csv('symbol_load.csv')
+    df = pd.read_csv('{}/symbol_load.csv'.format(mainfolder))
     for i in range(len(df)):
         try:
             db.insert(df.name[i], df.exchange[i], df.ins_type[i], int(df.stfp[i]), int(df.ltfp[i]), \
